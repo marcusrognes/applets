c05a7fe13e6d5fb17ab438fa135dba7ef8733652
Marcus Rognes <marcus@smartlabs.no>
2016-01-11 15:34:10 +0100

    Added basic bootstrapper for applets

---
+++ basicLoader.js
@@ -0,0 +1,7 @@
+javascript:(function () {
+    var url = 'https://rawgit.com/marcusrognes/applets/master/wpengine.js';
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    document.body.appendChild(script);
+})();
